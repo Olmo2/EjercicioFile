d1789ebce6c4a093f115e3c4f793aef944578deb
Olmo2 <olmot.s@gmail.com>
2020-11-05 14:04:43 +0100

    ejercicio 14

--- src/com/olmo/principal/Binarios1_Ejercicio13.java
+++ src/com/olmo/principal/Binarios1_Ejercicio13.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /* EJERCICIO 13*/
-public class Binarios1 {
+public class Binarios1_Ejercicio13 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		FileOutputStream fos = null;
